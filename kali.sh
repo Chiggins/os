4d5d8fb6efada94518d3ce65393e65b232d273ce
Chris Higgins <chris@chigs.me>
2016-11-23 15:12:02 -0600

    No need to install RVM because it'll break the install metasploit framework. Just install rvm and clone metasploit-framework manually if necessary

--- kali.sh
+++ kali.sh
@@ -14,11 +14,6 @@ wget https://github.com/Chiggins/DotFiles/raw/master/zsh/.zshrc -O ~/.zshrc --no
 wget https://github.com/Chiggins/DotFiles/raw/master/zsh/chiggins.zsh-theme -O ~/.oh-my-zsh/themes/chiggins.zsh-theme --no-check-certificate
 chsh -s /usr/bin/zsh
 
-# Install RVM
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable --ruby
-ln -s /usr/local/rvm/ ~/.rvm
-
 [ -e /usr/share/wordlists/rockyou.txt.gz ] && gzip -dc < /usr/share/wordlists/rockyou.txt.gz > /usr/share/wordlists/rockyou.txt
 
 git clone https://github.com/leebaird/discover /opt/discover/
