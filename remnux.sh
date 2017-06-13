e1ca56059902ad8bc8689dd992f3291cc1922033
Chris Higgins <chris@chigs.me>
2017-06-12 22:36:12 -0500

    Add vim and tmux additions to remnux

--- remnux.sh
+++ remnux.sh
@@ -2,15 +2,26 @@
 # Chiggins bootstrap script for Remnux
 
 # Install zsh, oh-my-zsh, and custom theme
-apt-get install -y zsh ctags
+apt-get install -y zsh ctags tmux vim
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 if [ -d ~/.zshrc ]
 then
-	rm ~/.zshrc
+    rm ~/.zshrc
 fi
 wget https://github.com/Chiggins/DotFiles/raw/master/zsh/.zshrc -O ~/.zshrc --no-check-certificate
 wget https://github.com/Chiggins/DotFiles/raw/master/zsh/chiggins.zsh-theme -O ~/.oh-my-zsh/themes/chiggins.zsh-theme --no-check-certificate
 chsh -s /usr/bin/zsh
+
+echo 'runtime vimrc' > ~/.vimrc
+if [ ! -d ~/.vim ]
+then
+    mkdir ~/.vim
+fi
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
+wget https://github.com/Chiggins/DotFiles/raw/master/vim/vimrc -O ~/.vimrc --no-check-certificate
+vim +PluginInstall +qall
+
+wget https://raw.githubusercontent.com/Chiggins/DotFiles/master/tmux/.tmux.conf -O ~/.tmux.conf --no-check-certificate
 
 # Install RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
