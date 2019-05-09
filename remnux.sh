5ac245bf00628a1898700b9a65771b92a4cff446
Chris Higgins <chris@chigs.me>
2019-05-09 11:35:46 -0500

    Update remnux.sh

--- remnux.sh
+++ remnux.sh
@@ -24,7 +24,7 @@ vim +PluginInstall +qall
 wget https://raw.githubusercontent.com/Chiggins/DotFiles/master/general/tmux.conf -O ~/.tmux.conf --no-check-certificate
 
 # Install RVM
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 # Remove this script
